64a923bef1f70a92f35b4eb40f0f431beeb436bf
Roman Kryvobok <fast.kamaz@gmail.com>
2026-09-18 16:39:44 +0300

    update vite and .gitignore for public page

--- httpdocs/backend/vite.config.ts
+++ httpdocs/backend/vite.config.ts
@@ -1,21 +1,21 @@
-import tailwindcss from '@tailwindcss/vite'
-import laravel from 'laravel-vite-plugin'
+import tailwindcss      from '@tailwindcss/vite'
+import laravel          from 'laravel-vite-plugin'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  build: {
-    sourcemap: true,
-  },
-  plugins: [
-    laravel({
-      input: [
-          'resources/assets/filament/css/admin/theme.css'
-      ],
-      refresh: [
-          'resources/views/**',
-        'app/Filament/**'
-      ],
-    }),
-    tailwindcss(),
-  ],
+    build:   {
+        sourcemap: true,
+    },
+    plugins: [
+        laravel({
+            input:   [
+                'resources/assets/filament/css/admin/theme.css'
+            ],
+            refresh: [
+                'resources/views/**',
+                'app/Filament/**'
+            ],
+        }),
+        tailwindcss(),
+    ],
 })
